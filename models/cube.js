33a91e5bbc697af752a2ff29275776d1d04a17f6
Lori Lynn Aoki <66411788+LoriLynn777@users.noreply.github.com>
2021-02-01 03:09:09 -1000

    initial_commit

---
+++ models/cube.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const Accessory = require('./accessory');
+
+const cubeSchema = new Schema ({
+    _id: Number,
+    name: String,
+    description: String,
+    imageUr1: String,
+    level: { type: String, required: true },
+    accessories: [{ type: Schema.Types.ObjectId, ref: 'Accessory' }]
+});
+
+const Cube = mongoose.model("Cube", cubeSchema);
+
+module.exports = Cube;
